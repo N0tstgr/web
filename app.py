f43be246461ff0f4cda015d4f89693d476801427
N0tstgr <vikashkasaudhan321@gmail.com>
2025-10-01 16:29:32 +0530

    1stProjectLoginFlask

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,  redirect,request, url_for, session, Response
 
 app = Flask(__name__)
+app.secret_key = "supersecret"
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -15,10 +16,29 @@ def login():
             return Response("IN-valid credentials. Try again", mimetype = "text/plain")
 
     return '''
-<h2> Login Page <h2>
-<form method = "POST"
-Username: <input type="text" name="username"><br>
-Password: <input type="text" name=password"><br>
+<h2> Login Page </h2>
+<form method = "POST">
+Username: <input type="text" name="username" ><br>
+Password: <input type="text" name="password"><br>
 <input type="submit" value="Login">
-
+</form>
 '''
+
+#welcome page after login
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f'''
+<h2>Welcome, {session["user"]}!</h2>
+<a href={url_for('logout')}>Logout</a>
+'''
+    
+    return redirect(url_for("login"))
+
+#logout route
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))   
+
+#Project 1 done
