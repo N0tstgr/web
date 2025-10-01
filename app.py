9824504fa4b4c54da3714d787efadd1e5efc8119
N0tstgr <vikashkasaudhan321@gmail.com>
2025-10-01 22:43:26 +0530

    login

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/submit", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
