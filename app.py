38139c80f14c861d045c4961d4ad4f57876c8b77
N0tstgr <vikashkasaudhan321@gmail.com>
2025-10-01 12:43:56 +0530

    add.login

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return 'Hello user ! this is my first flask app'
+
+@app.route("/about")
+def aboutus():
+    return 'we provide the best solutions for your problems'
+
+@app.route("/contact")
+def contact():
+    return 'contact us for your queries'
+
+@app.route("/login")
+def login():
+    return 'login/register on this page'
