0ce4241067139706bd931b16ce390107435a8d29
N0tstgr <vikashkasaudhan321@gmail.com>
2025-10-01 15:41:33 +0530

    loginPinFlask

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-from flask import Flask, request
+from flask import Flask,  redirect,request, url_for, session, Response
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return 'Hello user ! this is my first flask app'
-
-@app.route("/about")
-def aboutus():
-    return 'we provide the best solutions for your problems'
-
-@app.route("/contact")
-def contact():
-    return 'contact us for your queries'
-
-@app.route("/login")
+@app.route("/", methods=["GET", "POST"])
 def login():
-    return 'login/register on this page'
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-@app.route("/submit", methods = ["GET", "POST"])
-def submit():
-    if request.method== "POST":
-     return " you send data"
-    else:
-      return  "you are only viewing the form"
+        if username == "admin" and password =="123":
+            session["user"] = username #store in session
+            return redirect(url_for("welcome"))
+        else:
+            return Response("IN-valid credentials. Try again", mimetype = "text/plain")
+
+    return '''
+<h2> Login Page <h2>
+<form method = "POST"
+Username: <input type="text" name="username"><br>
+Password: <input type="text" name=password"><br>
+<input type="submit" value="Login">
+
+'''
